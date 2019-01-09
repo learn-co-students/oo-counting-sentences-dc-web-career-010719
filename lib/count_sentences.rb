8dcc67021c2399fa72e133a4e457e9b6cf9a4f98
github username <github email address>
2019-01-09 17:47:05 -0500

    monkey see monkey ccount the sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[.!?]/)
+    arr.each do |elem|
+      if elem == ""
+        arr.delete(elem)
+      end
+    end
+    arr.size
   end
 end
