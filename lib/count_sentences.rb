0e17dfa0ab2214350c9d66a270c50fed818ba291
James Clement <jameseclement@gmail.com>
2019-01-09 23:06:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+   
+  self.split(/[\.+!+\?+]/)
+  no_empty_sentences = self.split(/[\.!\?]/).reject { |s| s.empty? }
+  no_empty_sentences.count
+  
   end
 end
