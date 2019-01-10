e214dab917410daebe49ebf28560804665214f32
canikwe <c.anikwe@gmail.com>
2019-01-09 20:48:09 -0500

    Complete 9JAN19

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	# binding.pry
+  	del = [".", "?", "!"]
+  	count = self.split(Regexp.union(del)) #looked up documentaion on splitting by specific characters
+  	count.delete("")
+  	count.length
   end
 end
