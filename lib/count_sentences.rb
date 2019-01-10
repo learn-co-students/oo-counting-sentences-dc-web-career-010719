41cec19830c06ca740be7762ea7ae210e483aa81
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-01-10 16:13:12 -0500

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    if self.length > 0
+    period_sentences = self.split(".").delete_if { |element| element == '' }
+    count = period_sentences.size
+    excla_sentences = self.split("!").delete_if { |element| element == '' }
+    count = count + excla_sentences.size - 1
+    question_sentences = self.split("?").delete_if { |element| element == '' }
+    count = count + question_sentences.size - 1
+    else
+    return 0
+    end
   end
 end
